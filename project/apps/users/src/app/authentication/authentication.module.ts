9babd6e135140d73c17b834af23d2c382481db8b
mibu5 <mikolaj.burawski@gmail.com>
2023-03-26 15:34:09 +0100

    create BlogUserMemoryRepository

--- project/apps/users/src/app/authentication/authentication.module.ts
+++ project/apps/users/src/app/authentication/authentication.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { BlogUserModule } from '../blog-user/blog-user.module';
+import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository';
 
 @Module({
+  imports: [BlogUserModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
-export class AuthenticationModule {}
+export class AuthenticationModule {
+  constructor(private readonly blogUserRepository: BlogUserMemoryRepository) {}
+}
--- project/apps/users/src/app/blog-user/blog-user-memory.repository.ts
+++ project/apps/users/src/app/blog-user/blog-user-memory.repository.ts
@@ -1,22 +1,24 @@
-import {CRUDRepository} from '@project/util/util-types';
-import {User} from '@project/shared/shared-types';
-import {BlogUserEntity} from './blog-user.entity';
+import { Injectable } from '@nestjs/common';
+import { CRUDRepository } from '@project/util/util-types';
+import { User } from '@project/shared/shared-types';
+import { BlogUserEntity } from './blog-user.entity';
 
+@Injectable()
 export class BlogUserMemoryRepository
   implements CRUDRepository<BlogUserEntity, string, User>
 {
-  private repository: {[key: string]: User} = {};
+  private repository: { [key: string]: User } = {};
 
   public async create(item: BlogUserEntity): Promise<User> {
-    const entry = {...item.toObject(), _id: crypto.randomUUID()};
+    const entry = { ...item.toObject(), _id: crypto.randomUUID() };
     this.repository[entry._id] = entry;
 
-    return {...entry};
+    return { ...entry };
   }
 
   public async findById(id: string): Promise<User> {
     if (this.repository[id]) {
-      return {...this.repository[id]};
+      return { ...this.repository[id] };
     }
 
     return null;
@@ -31,7 +33,7 @@ export class BlogUserMemoryRepository
       return null;
     }
 
-    return {...existUser};
+    return { ...existUser };
   }
 
   public async destroy(id: string): Promise<void> {
@@ -39,7 +41,7 @@ export class BlogUserMemoryRepository
   }
 
   public async update(id: string, item: BlogUserEntity): Promise<User> {
-    this.repository[id] = {...item.toObject(), _id: id};
+    this.repository[id] = { ...item.toObject(), _id: id };
     return this.findById(id);
   }
 }
