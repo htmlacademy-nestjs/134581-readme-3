e1688ce68cc834017bd23bda68a50fd9179827e5
mibu5 <mikolaj.burawski@gmail.com>
2023-03-26 18:28:30 +0100

    Add register method

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import dayjs from 'dayjs';
+import { CreateUserDto } from './dto/create-user.dto';
+import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository';
+import { BlogUserEntity } from '../blog-user/blog-user.entity';
+import { AUTH_USER_EXISTS } from './authentication.constant';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(private readonly blogUserRepository: BlogUserMemoryRepository) {}
+
+  public async register(dto: CreateUserDto) {
+    const { email, firstname, lastname, password, dateBirth } = dto;
+
+    const blogUser = {
+      email,
+      firstname,
+      lastname,
+      avatar: '',
+      dateBirth: dayjs(dateBirth).toDate(),
+      passwordHash: '',
+    };
+
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
+
+    return this.blogUserRepository.create(userEntity);
+  }
+}
